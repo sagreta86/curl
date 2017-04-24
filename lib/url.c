1f12fa35cb9138e1cf45b6bed7f4f47c66e3d1b3
Daniel Stenberg <daniel@haxx.se>
2017-04-25 00:09:22 +0200

    CURLOPT_BUFFERSIZE: 1024 bytes is now the minimum size
    The buffer is needed to receive FTP, HTTP CONNECT responses etc so
    already at this size things risk breaking and smaller is certainly not
    wise.

--- lib/url.c
+++ lib/url.c
@@ -2284,22 +2284,24 @@ CURLcode Curl_setopt(struct Curl_easy *data, CURLoption option,
      * The application kindly asks for a differently sized receive buffer.
      * If it seems reasonable, we'll use it.
      */
-    data->set.buffer_size = va_arg(param, long);
+    arg = va_arg(param, long);
 
-    if(data->set.buffer_size > MAX_BUFSIZE)
-      data->set.buffer_size = MAX_BUFSIZE; /* huge internal default */
-    else if(data->set.buffer_size < 1)
-      data->set.buffer_size = BUFSIZE;
+    if(arg > MAX_BUFSIZE)
+      arg = MAX_BUFSIZE; /* huge internal default */
+    else if(arg < 1)
+      arg = BUFSIZE;
+    else if(arg < MIN_BUFSIZE)
+      arg = BUFSIZE;
 
     /* Resize only if larger than default buffer size. */
-    if(data->set.buffer_size > BUFSIZE) {
-      data->state.buffer = realloc(data->state.buffer,
-                                   data->set.buffer_size + 1);
+    if(arg > BUFSIZE) {
+      data->state.buffer = realloc(data->state.buffer, arg + 1);
       if(!data->state.buffer) {
         DEBUGF(fprintf(stderr, "Error: realloc of buffer failed\n"));
         result = CURLE_OUT_OF_MEMORY;
       }
     }
+    data->set.buffer_size = arg;
 
     break;
 
